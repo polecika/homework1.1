01447d3fd682ccb51332cf0daf9c778dd8df442d
Polina Svet <polecika1@gmail.com>
2020-06-21 21:12:22 +0300

    - добавила переключение экранов

--- app/src/main/java/com/example/androidshvets/SplashScreenActivity.java
+++ app/src/main/java/com/example/androidshvets/SplashScreenActivity.java
@@ -2,7 +2,10 @@ package com.example.androidshvets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.concurrent.TimeUnit;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        goToSecondScreen();
+    }
+
+    protected void goToSecondScreen() {
+        Intent intent = new Intent(SplashScreenActivity.this, FirstActivity.class);
+        try {
+            TimeUnit.SECONDS.sleep(1);
+            startActivity(intent);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
