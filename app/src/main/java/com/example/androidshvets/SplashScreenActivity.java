ca3e1fb3912f769f72bf75dd0d7bbdc822dc9678
Polina Svet <polecika1@gmail.com>
2020-06-24 12:33:40 +0300

    - исправила переключение экранов через потоки

--- app/src/main/java/com/example/androidshvets/SplashScreenActivity.java
+++ app/src/main/java/com/example/androidshvets/SplashScreenActivity.java
@@ -4,10 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.SystemClock;
 
 import java.util.concurrent.TimeUnit;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private static final long DELAY_MILLIS = 1500;
+    private Handler handler = new Handler();
+    private Runnable callback = new Runnable() {
+
+        @Override
+        public void run() {
+            Intent intent = new Intent(SplashScreenActivity.this, FirstActivity.class);
+            startActivity(intent);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +30,13 @@ public class SplashScreenActivity extends AppCompatActivity {
         goToSecondScreen();
     }
 
-    protected void goToSecondScreen() {
-        Intent intent = new Intent(SplashScreenActivity.this, FirstActivity.class);
-        try {
-            TimeUnit.SECONDS.sleep(1);
-            startActivity(intent);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    private void goToSecondScreen() {
+        handler.postDelayed(callback, DELAY_MILLIS);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(callback);
     }
 }
